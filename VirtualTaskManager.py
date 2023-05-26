b9e1f36a22ae48a88d70be2ccd3c5d32abed7504
PJiyush <jhamnani.1@iitj.ac.in>
2023-05-26 15:55:55 +0530

    Implemented the listen functionality

--- VirtualTaskManager.py
+++ VirtualTaskManager.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from gtts import gTTS
 import pygame as pg
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
 
 def speakFile():
     f = open('./test.txt', 'r')
     text  = str(f.read())
     f.close() # I havve to look this also
-
     strText = ""
-
     for i in text:
         if i == "\n":
             strText += " "
@@ -23,6 +25,36 @@ def speak(text):
     pg.mixer.music.play()
     
 
+def listen():
+    with sr.Microphone() as source:
+        print("Say Something")
+        audio = r.listen(source)
+        print("Done")
+    try:
+        text = r.recognize_google(audio)
+        print("You said: " + text)
+        # appending the text into the file
+        f = open('./test.txt', 'a')
+        f.write(text)
+        f.close()
+    except Exception as e:
+        print("I was not able to understad please repeat")
+        print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 root = Tk()
 root.geometry("140x120")
 myLabel = Label(root, text="I am your small agent")
@@ -30,7 +62,7 @@ myLabel.pack()
 
 myButton1 = Button(root, text="Tell Me", bg="red", command=speakFile)
 myButton1.config(height=2, width=10)
-myButton2 = Button(root, text="Listen", bg="blue")
+myButton2 = Button(root, text="Listen", bg="blue",command=listen)
 myButton2.config(height=2, width=10)
 myButton1.pack()
 myButton2.pack()
